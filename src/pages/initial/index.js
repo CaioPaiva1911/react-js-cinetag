78cb17bd953bb0e1b9b077f2631ef05398d3f008
Caio Paiva <caiowick@hotmail.com>
2025-08-11 20:43:23 -0300

    implement componentes card and title

--- src/pages/initial/index.js
+++ src/pages/initial/index.js
@@ -1,10 +1,18 @@
+import Banner from "components/Banner";
 import Cabecalho from "components/Cabecalho";
+import Card from "components/Card";
 import Rodape from "components/Rodape";
+import Titulo from "components/Titulo";
 
 function Inicio() {
     return (
         <>
             <Cabecalho />
+            <Banner imagem="Home" />
+            <Titulo>
+                <h1>A place to save your videos and movies!</h1>
+            </Titulo>
+            <Card id='1' titulo='Legend of the Guardians' capa='https://thecatapi.com/api/images/get?format=src&type=png' />
             <Rodape />
         </>
     )
