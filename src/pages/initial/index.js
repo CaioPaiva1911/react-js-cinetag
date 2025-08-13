397b87f5b33f9707123c6580a2ca4d90905433b8
Caio Paiva <caiowick@hotmail.com>
2025-08-12 21:46:11 -0300

    create context and implement like function

--- src/pages/initial/index.js
+++ src/pages/initial/index.js
@@ -1,19 +1,21 @@
 import Banner from "components/Banner";
-import Cabecalho from "components/Cabecalho";
 import Card from "components/Card";
-import Rodape from "components/Rodape";
 import Titulo from "components/Titulo";
+import videos from 'json/db.json';
+import styles from './Initial.module.css';
 
 function Inicio() {
     return (
         <>
-            <Cabecalho />
             <Banner imagem="Home" />
             <Titulo>
                 <h1>A place to save your videos and movies!</h1>
             </Titulo>
-            <Card id='1' titulo='Legend of the Guardians' capa='https://thecatapi.com/api/images/get?format=src&type=png' />
-            <Rodape />
+            <section className={styles.container}>
+                {videos.map((video) => {
+                    return <Card {...video} key={video.id} />
+                })}
+            </section>
         </>
     )
 }
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,24 @@
+import Cabecalho from "components/Cabecalho";
+import Container from "components/Container";
+import Rodape from "components/Rodape";
+import FavoritosProvider from "contextos/Favoritos";
+import Favoritos from "pages/Favoritos";
 import Inicio from "pages/initial";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function AppRoutes() {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Inicio />}></Route>
-            </Routes>
+            <Cabecalho />
+            <Container>
+                <FavoritosProvider>
+                    <Routes>
+                        <Route path="/" element={<Inicio />}></Route>
+                        <Route path="/favoritos" element={<Favoritos />}></Route>
+                    </Routes>
+                    <Rodape />
+                </FavoritosProvider>
+            </Container>
         </BrowserRouter>
     )
 }
